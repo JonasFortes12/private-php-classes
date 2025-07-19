f0a30854e40acca5d1f081879d647ee69b37bfdc
Jonas Fortes <jonascforte@hotmail.com>
2025-07-19 10:15:01 -0300

    show teorical

---
+++ 1. introducao-logica-programacao/1.2-estruturas-repeticao/repeticao.php
@@ -0,0 +1,45 @@
+<?php
+
+// Em php temos 3 estruturas principais:
+// for , while, foreach
+
+/*O laço for: Usado quando eu sei 
+ exatamente quantas vezes quero executar
+ um bloco de código */
+// 1º parâmtro: Inicialização
+// 2º parâmetro: Condição(de parada)
+// 3º parâmetro: Incremento
+
+ for($i = 0; $i < 5; $i=$i+1 ){
+    echo "\nNúmero: $i";
+ } 
+
+//  i=0 -> i=1 -> i=2 -> i=3 -> i=4 : i=5
+
+
+/**
+ * O laço While: Eu uso quando eu não sei quantas vezes
+ * o bloco de código deve ser repetido.
+ * 1 parâmetro: A condicão de parada.
+ */
+ $i = 0;
+ while($i < 5) {
+    echo "\nNumero: $i";
+    $i = $i + 1;
+ }
+
+/**
+ * A estrura foreach é usada para [iterar] sobre uma
+ * lista/array de elementos ou objetos.
+ * É usada quando eu preciso percorrer/"varrer"
+ * 
+ */
+
+$arrayFrutas = ["maça", "banana", "uva"];
+        //  0        1        2
+
+
+foreach(["maça", "banana", "uva"] as $fruta){
+    echo "\nFruta atual: $fruta ";
+}
+
